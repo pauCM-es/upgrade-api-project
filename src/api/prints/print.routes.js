5dd970ac065c66344cc9ec30588298dc3b45a88e
pauCM-es <costamingarro@gmail.com>
2023-01-23 09:27:01 +0100

    cloudinary but doesn't work

--- src/api/prints/print.routes.js
+++ src/api/prints/print.routes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const isAuth = require('../../utils/auth/middlewares/authMiddlewares');
 const controller = require('./print.controllers');
 const router = express.Router();
+const {upload} = require('../../middleware/files.middleware');
 
 router.get('/', controller.getPrints);
 router.get('/:id', controller.getPrint);
-router.post('/create', isAuth, controller.createPrint);
-router.put('/edit/:id', isAuth, controller.editPrint);
+router.post('/create', [isAuth, upload.single('images')], controller.createPrint);
+router.put('/edit/:id', [isAuth], controller.editPrint);
 router.delete('/delete/:id', isAuth, controller.deletePrint);
 
 module.exports = router;
